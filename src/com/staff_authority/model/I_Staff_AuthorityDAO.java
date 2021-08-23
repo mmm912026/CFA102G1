53b109d9511acc977c8cf67f47ad9ceb5f934912
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-08-23 16:24:18 +0800

    重新上傳(2) add table : STAFF_AUTHORITY(員工權限)

--- src/com/staff_authority/model/I_Staff_AuthorityDAO.java
+++ src/com/staff_authority/model/I_Staff_AuthorityDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 
 public interface I_Staff_AuthorityDAO {
-	void insert(Staff_AuthorityVO staff_authouity);
-	Staff_AuthorityVO findByStaff_no(int STAFF_NO);
-    List<Staff_AuthorityVO> getAll();
+	public void insert(Staff_AuthorityVO staff_authouity);
+	public Staff_AuthorityVO findByStaff_no(int STAFF_NO);
+	public List<Staff_AuthorityVO> getAll();
 }
