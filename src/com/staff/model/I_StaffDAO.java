cd9150e168d23a13ec3c9a1a3991cf796ad75fd3
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-08-23 18:26:28 +0800

    重新上傳(3) add table : STAFF(員工)

--- src/com/staff/model/I_StaffDAO.java
+++ src/com/staff/model/I_StaffDAO.java
@@ -6,9 +6,9 @@ import java.util.List;
 public interface I_StaffDAO {
 	public void insert(StaffVO staff);
 	public void update(StaffVO staff);
-	public void delete(int STAFF_NO);
-	public StaffVO findByStaff_no(int STAFF_NO);
+	public void delete(Integer STAFF_NO);
+	public StaffVO findByStaff_no(Integer STAFF_NO);
 	public List<StaffVO> findByStaff_name(String STAFF_NAME);
-	public List<StaffVO> findByStaff_phone(int STAFF_PHONE);
+	public List<StaffVO> findByStaff_phone(Integer STAFF_PHONE);
 	public List<StaffVO> getAll();
 }
