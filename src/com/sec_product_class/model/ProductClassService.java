220e21a0b292b0af6547722e155118faf1f5d135
LAPTOP-6IR4E4AF\Tibame_T14 <mmm912026@gmail.com>
2021-08-28 23:36:45 +0800

    add ProductClassService

---
+++ src/com/sec_product_class/model/ProductClassService.java
@@ -0,0 +1,39 @@
+package com.sec_product_class.model;
+
+import java.util.List;
+
+public class ProductClassService{
+	private I_ProductClassDAO dao;
+	
+	public ProductClassService() {
+		dao = new ProductClassJDBCDAO();
+	}
+
+	public ProductClassVO addProductClass(String spc_name) {
+		
+		ProductClassVO productClassVO = new ProductClassVO();
+		productClassVO.setSpc_name(spc_name);
+		productClassVO = dao.insert(productClassVO);
+		return productClassVO;
+	}
+
+	public ProductClassVO updateProductClass(Integer spc_no, String spc_name) {
+		ProductClassVO productClassVO = new ProductClassVO();
+		productClassVO.setSpc_no(spc_no);
+		productClassVO.setSpc_name(spc_name);
+		dao.update(productClassVO);
+		return productClassVO;
+	}
+
+	public void deleteProductClass(Integer spc_no) {
+		dao.delete(spc_no);
+	}
+
+	public ProductClassVO findByPKProductClass(Integer spc_no) {
+		return dao.findByPK(spc_no);
+	}
+
+	public List<ProductClassVO> getAll() {
+		return dao.getAll();
+	}
+}
