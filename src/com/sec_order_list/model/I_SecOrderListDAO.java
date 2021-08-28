b43fb1a1d76e513827a1d43418f944ee87163bc5
LAPTOP-6IR4E4AF\Tibame_T14 <mmm912026@gmail.com>
2021-08-28 23:11:04 +0800

    修改:SEC_ORDER_LIST(訂單明細) 1.增加insert後回傳自增主鍵PK 2.只需要關閉con，將其餘關閉的程式刪除

--- src/com/sec_order_list/model/I_SecOrderListDAO.java
+++ src/com/sec_order_list/model/I_SecOrderListDAO.java
@@ -3,7 +3,7 @@ package com.sec_order_list.model;
 import java.util.List;
 
 public interface I_SecOrderListDAO {
-	public void insert(SecOrderListVO secOrderList);
+	public SecOrderListVO insert(SecOrderListVO secOrderList);
 	public void update(SecOrderListVO secOrderList);
 	public void delete(Integer sol_no);
 	public SecOrderListVO findByPk(Integer sol_no);
--- src/com/sec_order_list/model/SecOrderListJDBCDAO.java
+++ src/com/sec_order_list/model/SecOrderListJDBCDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,31 +27,30 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 	
 
 	@Override
-	public void insert(SecOrderListVO secOrderList) {
+	public SecOrderListVO insert(SecOrderListVO secOrderList) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
 		try {
 			Class.forName(DRIVER);
 			con = DriverManager.getConnection(URL, USER, PASSWORD);
-			pstmt = con.prepareStatement(INSERT_STMT);
+			pstmt = con.prepareStatement(INSERT_STMT,Statement.RETURN_GENERATED_KEYS);
 			pstmt.setInt(1, secOrderList.getSo_no());
 			pstmt.setInt(2, secOrderList.getSpi_no());
 			pstmt.setInt(3, secOrderList.getSol_proamot());
 			pstmt.setInt(4, secOrderList.getSol_pri());
 			pstmt.executeUpdate();
+			
+			rs = pstmt.getGeneratedKeys();
+			if(rs.next()) {
+				secOrderList.setSol_no(rs.getInt(1));
+			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 			if(con != null) {
 				try {
 					con.close();
@@ -59,6 +59,7 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 				}
 			}
 		}
+		return secOrderList;
 	}
 
 	@Override
@@ -82,13 +83,6 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 			if(con != null) {
 				try {
 					con.close();
@@ -116,13 +110,6 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 			if(con != null) {
 				try {
 					con.close();
@@ -160,20 +147,6 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 			if(con != null) {
 				try {
 					con.close();
@@ -213,20 +186,6 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 			if(con != null) {
 				try {
 					con.close();
@@ -239,18 +198,19 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 	}
 
 	//測試驗證>>>>>>>>>>>>>>>>>>>>>>>>>>
-			public static void main (String[] args) {
-				
+//			public static void main (String[] args) {
+//				
 //				//測試INSERT(OK).........................................
 //				SecOrderListJDBCDAO dao = new SecOrderListJDBCDAO();
 //				SecOrderListVO secOrderList = new SecOrderListVO();
-//				secOrderList.setSo_no(3);
-//				secOrderList.setSpi_no(2);
+//				secOrderList.setSo_no(1);
+//				secOrderList.setSpi_no(1);
 //				secOrderList.setSol_proamot(4);
 //				secOrderList.setSol_pri(150000);
-//				dao.insert(secOrderList);
+//				secOrderList = dao.insert(secOrderList);
+//				System.out.println(secOrderList.getSol_no());
 //				System.out.println("OK!!");
-				
+//				
 //				//測試UPDATE(OK).........................................
 //				SecOrderListJDBCDAO dao = new SecOrderListJDBCDAO();
 //				SecOrderListVO secOrderList = new SecOrderListVO();
@@ -261,12 +221,12 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 //				secOrderList.setSol_pri(30000);
 //				dao.update(secOrderList);
 //				System.out.println("OK!!");
-				
+//				
 //				//測試DELETE(OK).........................................
 //				SecOrderListJDBCDAO dao = new SecOrderListJDBCDAO();
 //				dao.delete(3);
 //				System.out.println("OK!!");
-
+//
 //				//測試FIND_BY_PK(OK).........................................
 //				SecOrderListJDBCDAO dao = new SecOrderListJDBCDAO();
 //				SecOrderListVO secOrderList = dao.findByPk(2);
@@ -275,7 +235,7 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 //				System.out.println(secOrderList.getSpi_no() + ", ");
 //				System.out.println(secOrderList.getSol_proamot() + ", ");
 //				System.out.println(secOrderList.getSol_pri() + ", ");
-
+//
 //				//測試GET_ALL(OK).........................................
 //				SecOrderListJDBCDAO dao = new SecOrderListJDBCDAO();
 //				List<SecOrderListVO> list = dao.getAll();
@@ -289,6 +249,6 @@ public class SecOrderListJDBCDAO implements I_SecOrderListDAO{
 //					System.out.println("................................................");
 //				}
 	//<<<<<<<<<<<<<<<<<<<<<<<<<測試驗證
-
-			}
+//
+//			}
 }
