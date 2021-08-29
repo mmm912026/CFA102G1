b327323c73a2eb29b7c6b611f330b04a9994fd38
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-08-29 13:42:51 +0800

    add Table : Staff_AuthorityService

---
+++ src/com/staff_authority/model/Staff_AuthorityService.java
@@ -0,0 +1,26 @@
+package com.staff_authority.model;
+
+import java.util.List;
+
+public class Staff_AuthorityService {
+private I_Staff_AuthorityDAO dao;
+	
+	public Staff_AuthorityService() {
+		dao = new Staff_AuthorityJDBCDAO();
+	}
+public Staff_AuthorityVO addStaff_Authority(
+		Integer staff_no, Integer authority_no) {
+	Staff_AuthorityVO staff_authority = new Staff_AuthorityVO();
+	
+	staff_authority.setStaff_no(staff_no);
+	staff_authority.setAuthority_no(authority_no);
+	dao.insert(staff_authority);
+	return staff_authority;
+}
+public Staff_AuthorityVO getOneStaff_Authority(Integer staff_no) {
+	return dao.findByStaff_no(staff_no);
+ }
+public List<Staff_AuthorityVO> getAll(){
+	return dao.getAll();
+	}
+}
