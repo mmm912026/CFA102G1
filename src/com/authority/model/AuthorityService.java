f923dfdcc755192036efe845b050abc8eef485ef
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-08-29 13:42:32 +0800

    add Table : AuthorityService

---
+++ src/com/authority/model/AuthorityService.java
@@ -0,0 +1,41 @@
+package com.authority.model;
+
+import java.util.List;
+
+public class AuthorityService {
+	private I_AuthorityDAO dao;
+	
+	public AuthorityService() {
+		dao = new AuthorityJDBCDAO();
+	}
+	
+	public AuthorityVO addAuthority(
+			Integer authority_no, String authority_name) {
+		
+		AuthorityVO authority = new AuthorityVO();
+		
+		authority.setAuthority_no(authority_no);
+		authority.setAuthority_name(authority_name);
+		dao.insert(authority);
+		return authority;
+	}
+	public AuthorityVO updateAuthority(
+			Integer authority_no, String authority_name) {
+		AuthorityVO authority = new AuthorityVO();
+		
+		authority.setAuthority_no(authority_no);
+		authority.setAuthority_name(authority_name);
+		dao.update(authority);
+	    return authority;
+	}
+	public void deleteAuthority(Integer authority_no) {
+		dao.delete(authority_no);
+	}
+	public AuthorityVO getOneAuthority(Integer authority_no) {
+		return dao.findByAuthority_no(authority_no);
+		}
+	public List<AuthorityVO> getAll(){
+		return dao.getAll();
+		
+	}
+}
