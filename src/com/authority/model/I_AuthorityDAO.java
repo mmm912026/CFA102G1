85161c8942fe2c5c70cb4be7cb2b57ff1f37901a
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-08-29 18:24:22 +0800

    修改_02  Table : Authority(功能) 錯字修正

--- src/com/authority/model/I_AuthorityDAO.java
+++ src/com/authority/model/I_AuthorityDAO.java
@@ -3,9 +3,9 @@ package com.authority.model;
 import java.util.List;
 
 public interface I_AuthorityDAO {
-	public AuthorityVO insert(AuthorityVO authouity);
+	public AuthorityVO insert(AuthorityVO authority);
 	public void delete(Integer authority_no);
-	public void update(AuthorityVO authouity);
+	public void update(AuthorityVO authority);
 	public AuthorityVO findByAuthority_no(Integer authority_no);
 	public List<AuthorityVO> getAll();
 	   
