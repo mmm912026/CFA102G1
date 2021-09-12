14605dbd4886485477a920560716133cc5070f31
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-09-12 13:18:23 +0800

    add Table : Staff (員工) 新增 控制 連線池 修改 電話 的類別

--- src/com/staff/model/I_StaffDAO.java
+++ src/com/staff/model/I_StaffDAO.java
@@ -2,13 +2,16 @@ package com.staff.model;
 
 import java.util.List;
 
+import com.member.model.MemberVO;
+
 
 public interface I_StaffDAO {
 	public StaffVO insert(StaffVO staff);
 	public void update(StaffVO staff);
 	public void delete(Integer stff_no);
 	public StaffVO findByStaff_no(Integer staff_no);
+	public StaffVO findByStaff_account(String staff_account, String staff_password);
 	public List<StaffVO> findByStaff_name(String staff_name);
-	public List<StaffVO> findByStaff_phone(Integer staff_phone);
+	public List<StaffVO> findByStaff_phone(String staff_phone);
 	public List<StaffVO> getAll();
 }
