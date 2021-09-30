32c04a1e6866d171011a66188be9e9d1aeefaf48
asd56494783 <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2021-09-30 10:49:48 +0800

    修改成驗證碼 新增 驗證功能 查詢

--- src/com/maintence_case_img/model/Maintence_Case_ImgService.java
+++ src/com/maintence_case_img/model/Maintence_Case_ImgService.java
@@ -10,18 +10,17 @@ public class Maintence_Case_ImgService {
 		dao = new Maintence_Case_ImgDAO();
 	}
 	
-	public Maintence_Case_ImgVO insertMaintenceCaseImg(Integer mca_no, byte[] mci_before_img,byte[] mci_after_img ) {
+	public Maintence_Case_ImgVO insertMaintenceCaseImg(Integer mca_no, String mci_before_img ) {
 		
 		Maintence_Case_ImgVO maintenceCaseImgVO = new Maintence_Case_ImgVO();
 		
 		maintenceCaseImgVO.setMca_no(mca_no);
 		maintenceCaseImgVO.setMci_before_img(mci_before_img);
-		maintenceCaseImgVO.setMci_after_img(mci_after_img);
 		maintenceCaseImgVO = dao.insert(maintenceCaseImgVO);
 		return maintenceCaseImgVO;
 	}
 
-	public Maintence_Case_ImgVO updateMaintenceCaseImg(Integer mci_no,Integer mca_no,byte[] mci_before_img,byte[] mci_after_img ) {
+	public Maintence_Case_ImgVO updateMaintenceCaseImg(Integer mci_no,Integer mca_no,String mci_before_img,byte[] mci_after_img ) {
 		
 		Maintence_Case_ImgVO maintenceCaseImgVO = new Maintence_Case_ImgVO();
 		
@@ -37,8 +36,8 @@ public class Maintence_Case_ImgService {
 		dao.delete(mci_no);
 	}
 
-	public  Maintence_Case_ImgVO getOneMaintenceCaseImg(Integer mci_no) {
-		return dao.findByPK(mci_no);
+	public  Maintence_Case_ImgVO getOneMaintenceCaseImg(String mci_before_img) {
+		return dao.findByPK(mci_before_img);
 	}
 
 	public List<Maintence_Case_ImgVO> getAll() {
